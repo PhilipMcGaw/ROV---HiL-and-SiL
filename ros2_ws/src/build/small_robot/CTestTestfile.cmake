421b0a9a2f65f2d2251fd80d469e7c9f602de0ff
Philip McGaw <philip@mcgaw.eu>
2026-08-15 21:26:42 +0100

    working on getting a small robot

---
+++ ros2_ws/src/build/small_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/skippy/ROV - HiL-and-SiL/ros2_ws/src/small_robot
+# Build directory: /home/skippy/ROV - HiL-and-SiL/ros2_ws/src/build/small_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
