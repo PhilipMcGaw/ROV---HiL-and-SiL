eeab1307a593985fbd662e15c2f15e9917488c8c
Skippy <philip@mcgaw.eu>
2026-08-16 15:51:39 +0100

    clean up

--- ros2_ws/src/build/small_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/skippy/ROV - HiL-and-SiL/ros2_ws/src/small_robot
-# Build directory: /home/skippy/ROV - HiL-and-SiL/ros2_ws/src/build/small_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
